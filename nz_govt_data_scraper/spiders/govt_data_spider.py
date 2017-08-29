976066c0f201ad58bff21a690857acc7b73c48ee
DamonTarlaei <merry.mike@gmail.com>
2017-08-29 18:49:05 +1200

    scraper now working

--- nz_govt_data_scraper/spiders/govt_data_spider.py
+++ nz_govt_data_scraper/spiders/govt_data_spider.py
@@ -13,16 +13,13 @@ class GovtDataSpider(scrapy.Spider):
 
         start_urls = ['https://catalogue.data.govt.nz']
         urls = [
-            'https://catalogue.data.govt.nz/group/fde0ba8b-cb3f-46d9-998d-b56cb7789b7f?_ga=2.33439038.1652006762.1503973375-684835626.1503973375&organization=ministry-of-health&page=1',
+            'https://catalogue.data.govt.nz/group/health',
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        # page = response.url.split("/")[-2]
-        # filename = 'quotes-%s.html' % page
-        # with open(filename, 'wb') as f:
-            # f.write(response.body)
+
         num_results = response.css('#group-datasets-search-form h2::text').extract_first().strip().split(" ")[0]
 
         logging.log(logging.INFO, num_results)
@@ -31,17 +28,13 @@ class GovtDataSpider(scrapy.Spider):
 
         logging.log(logging.INFO, 'About to try the OPFs')
 
-        counter = 0
         for opf in on_page_files:
-            counter += 1
+
             opf_url = response.urljoin(opf.css('a::attr(href)').extract()[0])
 
             logging.log(logging.INFO, 'Going into %s' % opf_url)
 
             yield scrapy.Request(opf_url, callback=self.file_parse)
-
-            # if counter > 0:
-            #     break
 
         next_page = response.css('.pagination.pagination-centered li.active + li>a::attr(href)').extract_first()
         if next_page is not None:
